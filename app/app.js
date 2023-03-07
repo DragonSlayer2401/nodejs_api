c2e950ee7092a5deedab0131f2568d6135f2bd3c
DragonSlayer2401 <jssmith2@student.fullsail.edu>
2023-03-06 23:34:52 -0500

    Finished files

--- app/app.js
+++ app/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const todosRouter = require("../router/todosRouter");
 
 const app = express();
 // http://localhost:3000
 app.get("/", (req, res, next) => {
     res.status(200).json({message:"Service is up"});
 });
-
+// router middleware
+app.use("/todos", todosRouter);
 // add middleware to handle errors and bad url paths
 app.use((req, res, next) => {
     const error = new Error("NOT FOUND!!");
@@ -20,7 +22,7 @@ app.use((error, req, res, next) => {
             status: error.status,
             method: req.method,
         }
-    })
+    });
 });
 
 module.exports = app;
